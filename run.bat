160814e6de3f8620b9dff6a82d5e2fe6c7dd7bb2
piotrjurga <benny.profane37@gmail.com>
2020-01-12 21:20:22 +0100

    update run.bat

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@
 set JSON_LIB=%HOMEDRIVE%%HOMEPATH%\.m2\repository\org\json\json\20190722\json-20190722.jar
 
 pushd target\classes
-java -classpath "%JSON_LIB%;." pl.put.poznan.scenario.ScenarioQualityChecker
+java -classpath "%JSON_LIB%;." pl.put.poznan.scenario.gui.ScenarioQualityCheckerGui
 popd
 
