1318e89de97ad3545b959aef933c2fe7c8ed8267
Marcin Gapiński <49092713+margap1998@users.noreply.github.com>
2020-01-12 20:08:29 +0100

    Update IncorrectStepsTests.java
    add 2 tests for scenarios, and draft of documentation

--- src/test/java/pl/put/poznan/scenario/logic/IncorrectStepsTests.java
+++ src/test/java/pl/put/poznan/scenario/logic/IncorrectStepsTests.java
@@ -2,8 +2,14 @@ package pl.put.poznan.scenario.logic;
 
 import org.junit.jupiter.api.*;
 
+/**
+ * Unit tests class for ScenarioIncorrectStepListVisitor.
+ */
 public class IncorrectStepsTests
 {
+    /**
+     * This test method checks reaction for 1 Step with correct name, that means, it starts with Actor{System or actors defined in Scenario}
+     */
     @Test
     public void testIncorrectStepsVisitorWithCorrectSimpleStep()
     {
@@ -15,6 +21,10 @@ public class IncorrectStepsTests
         mockStep.accept(testVis);
         Assertions.assertTrue(testVis.getIncorrectSteps().isEmpty());
     }
+
+    /**
+     * This test method checks reaction for 1 Step with non-defined actor at beginning.
+     */
     @Test
     public void testIncorrectStepsVisitorWithIncorrectSimpleStep()
     {
@@ -26,6 +36,10 @@ public class IncorrectStepsTests
         mockStep.accept(testVis);
         Assertions.assertEquals(mockStep.name,testVis.getIncorrectSteps().get(0));
     }
+
+    /**
+     * This method checks the behaviour of step starting with keyword "IF:" and incorrect actor.
+     */
     @Test
     public void testIncorrectStepsKeywordIFIncorrect2()
     {
@@ -37,6 +51,10 @@ public class IncorrectStepsTests
         mockStep.accept(testVis);
         Assertions.assertEquals(mockStep.name,testVis.getIncorrectSteps().get(0));
     }
+
+    /**
+     * This method checks the behaviour of step starting with keyword "ELSE:" and incorrect actor.
+     */
     @Test
     public void testIncorrectStepsKeywordELSEIncorrect2()
     {
@@ -48,6 +66,10 @@ public class IncorrectStepsTests
         mockStep.accept(testVis);
         Assertions.assertEquals(mockStep.name,testVis.getIncorrectSteps().get(0));
     }
+
+    /**
+     * This method checks the behaviour of step starting with keyword "FOR EACH" and incorrect actor.
+     */
     @Test
     public void testIncorrectStepsKeywordFOREACHIncorrect2()
     {
@@ -60,6 +82,9 @@ public class IncorrectStepsTests
         Assertions.assertEquals(mockStep.name,testVis.getIncorrectSteps().get(0));
     }
 
+    /**
+     * This method checks the behaviour of step starting with keyword "IF:" and correct actor.
+     */
     @Test
     public void testIncorrectStepsKeywordIFCorrect2()
     {
@@ -71,6 +96,10 @@ public class IncorrectStepsTests
         mockStep.accept(testVis);
         Assertions.assertTrue(testVis.getIncorrectSteps().isEmpty());
     }
+
+    /**
+     * This method checks the behaviour of step starting with keyword "ELSE:" and correct actor.
+     */
     @Test
     public void testIncorrectStepsKeywordELSECorrect2()
     {
@@ -82,6 +111,10 @@ public class IncorrectStepsTests
         mockStep.accept(testVis);
         Assertions.assertTrue(testVis.getIncorrectSteps().isEmpty());
     }
+
+    /**
+     * This method checks the behaviour of step starting with keyword "FOR EACH" and correct actor.
+     */
     @Test
     public void testIncorrectStepsKeywordFOREACHCorrect2()
     {
@@ -93,6 +126,10 @@ public class IncorrectStepsTests
         mockStep.accept(testVis);
         Assertions.assertTrue(testVis.getIncorrectSteps().isEmpty());
     }
+
+    /**
+     * This method checks the behaviour of step with null instead of empty string as name
+     */
     @Test
     public void testIncorrectStepsNull()
     {
@@ -103,5 +140,45 @@ public class IncorrectStepsTests
         mockStep.name = null;
         mockStep.accept(testVis);
         Assertions.assertFalse(testVis.getIncorrectSteps().isEmpty());
+    }
+
+    /**
+     * Tests behaviour for made-up scenario with correct steps
+     */
+    @Test
+    public void testCorrectScenario()
+    {
+        Scenario a = new Scenario();
+        a.title = "Bakery";
+        a.system = "System";
+        a.actors.add("User");
+        a.steps.add(new Step("User ..."));
+        a.steps.add(new Step("User ..."));
+        a.steps.add(new Step("System ..."));
+        a.steps.add(new Step("IF: User..."));
+        a.steps.get(3).substeps.add(new Step("System ..."));
+        ScenarioIncorrectStepListVisitor testVis = new ScenarioIncorrectStepListVisitor();
+        a.accept(testVis);
+        Assertions.assertTrue(testVis.getIncorrectSteps().isEmpty());
+    }
+
+    /**
+     * Tests behaviour for mede-up Scenario with 2 incorrect steps
+     */
+    @Test
+    public void testIncorrectScenario()
+    {
+        Scenario a = new Scenario();
+        a.title = "Bakery";
+        a.system = "System";
+        a.actors.add("User");
+        a.steps.add(new Step("User ..."));
+        a.steps.add(new Step("User ..."));
+        a.steps.add(new Step("The bread ..."));
+        a.steps.add(new Step("IF: Cakes..."));
+        a.steps.get(3).substeps.add(new Step("System ..."));
+        ScenarioIncorrectStepListVisitor testVis = new ScenarioIncorrectStepListVisitor();
+        a.accept(testVis);
+        Assertions.assertTrue(testVis.IncorrectSteps.contains("The bread ...")&& testVis.IncorrectSteps.contains("IF: Cakes..."));
     }
 }
